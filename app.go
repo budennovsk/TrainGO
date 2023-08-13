25f5d342c3c574ba44d11b3c276aa8fc680cfbb9
Python <svetloewill@mail.ru>
2023-08-13 19:45:24 +0300

    first serverw

--- app.go
+++ app.go
@@ -3,10 +3,50 @@ package main
 import "fmt"
 
 func main() {
-	a := 1
-	var b string = "hello"
+	fmt.Println(get_id())
+	id, s := get_id()
+	fmt.Println(id, s)
+	//fmt.Println(get_iter())
+	res()
 
-	fmt.Println("hello", a)
-	var c = fmt.Sprintf("my name is :%s AND vikki %d", b, a)
-	fmt.Println(c)
+}
+
+func get_id() (int, string) {
+	a := 1
+	b := "string"
+	return a, b
+}
+func get_iter() int {
+	sum := 0
+	for sum < 10 {
+
+		if sum == 2 {
+			fmt.Println(sum, "dd")
+		} else {
+			fmt.Println(sum)
+		}
+		sum++
+	}
+	return sum
+
+}
+
+func res() {
+	pe := 12
+	a := &pe
+	*a = 100
+	fmt.Println(*a, pe)
+	var liststr [3]string
+	liststr[0] = "1111"
+	liststr[1] = "2222"
+	liststr[2] = "3333"
+	createSlice := []string{}
+	createSlice = append(createSlice, "sexjnd", "ggg")
+	fmt.Println(createSlice)
+	fmt.Println(cap(createSlice), "fffffff")
+	for i := 0; i < len(liststr); i++ {
+		fmt.Println(liststr[i])
+
+	}
+
 }
